8c62a4aa53f97c9499a5e4ab2a1fa7e72d4ec740
kumok <bhh@byu.edu>
2015-01-24 10:22:20 -0700

    vevever

--- OptionsToolbar/OTViewController.m
+++ OptionsToolbar/OTViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "OTViewController.h"
+#import <MessageUI/MessageUI.h>
+#import "OTNewFancyViewController.h"
 
-@interface OTViewController ()
+@interface OTViewController () <MFMailComposeViewControllerDelegate>
 
 @end
 
@@ -24,6 +26,39 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)mailButtonTapped:(id)sender {
+    
+    if ([MFMailComposeViewController canSendMail]) {
+        
+        MFMailComposeViewController *mc = [MFMailComposeViewController new];
+        
+        mc.mailComposeDelegate = self;
+        
+        [self presentViewController:mc animated:YES completion:nil];
+    }
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (IBAction)redButtonTapped:(id)sender {
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (IBAction)blueButtonTapped:(id)sender {
+    
+    self.view.backgroundColor = [UIColor blueColor];
+}
+
+- (IBAction)newButtonTapped:(id)sender {
+    
+    OTNewFancyViewController *newViewController = [OTNewFancyViewController new];
+    
+    [self presentViewController:newViewController animated:YES completion:nil];
+    
 }
 
 @end
